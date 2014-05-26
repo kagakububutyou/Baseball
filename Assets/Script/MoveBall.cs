93acc396e33f693c130d52376c3b3f52863a0a33
shinnnosuke1232 <shinnnosuke1232@yahoo.co.jp>
2014-05-26 11:20:10 +0900

    カーブver1.01

--- Assets/Script/MoveBall.cs
+++ Assets/Script/MoveBall.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class MoveBall : MonoBehaviour {
 
-	private int speed		= -30; 		// 弾のスピード
+	public float speed		= -30.0f; 	// 弾のスピード
 
 	public float Curve		= 0.0f;		//	カーブの度合い
+
+	public bool IsHitBatt	= false;	//	バットに当たったか
 
 	public float LightCurve	=  7.0f;	//	右カーブ
 	public float LeftCurve	= -7.0f;	//	左カーブ
@@ -13,19 +15,24 @@ public class MoveBall : MonoBehaviour {
 	// スクリプトが有効になったとき一回だけ呼ばれます
 	void Start () 
 	{
+		IsHitBatt = false;
+
 		// ホームベース向きに速度を渡してあげます
 		rigidbody.velocity = new Vector3(speed, 0, speed);
 	}
 
 	// 毎フレーム呼ばれます
-	void Update () 
+	void Update ()
 	{
 
 
 		//	カーブ
 		Curve += Input.GetAxisRaw("Horizontal");
 
-		if(Input.GetAxisRaw("Horizontal") == 0)
+		if(Input.GetAxisRaw("Horizontal") != 0 && IsHitBatt == false)
+		{
+			rigidbody.velocity = new Vector3(speed, 0, speed - Curve);
+		}else
 		{
 			Curve = 0;
 		}
@@ -34,18 +41,21 @@ public class MoveBall : MonoBehaviour {
 		Debug.Log(Curve);
 
 		// ホームベース向きに速度を渡してあげます
-		rigidbody.velocity = new Vector3(0, 0, - Curve);
 	}
 
 	//	壁にあたったら
-	void OnCollisionEnter(Collision col)
+	void OnCollisionEnter(Collision collision)
 	{
-		if(col.gameObject.name == "Wall")
+		if(collision.gameObject.name == "Wall")
 		{
 			//	動きを止めて
 			rigidbody.velocity = Vector3.zero;
 			//	回転を止める
 			rigidbody.angularVelocity = Vector3.zero;
+		}
+		if(collision.gameObject.name == "Batter")
+		{
+			IsHitBatt = true;
 		}
 	}
 }
